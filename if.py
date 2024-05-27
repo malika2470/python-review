1462bc547a2bd91317af0128c01f922c116ff05d
malika2470 <malika2470@gmail.com>
2024-05-26 20:58:19 -0400

    ""

--- if.py
+++ if.py
@@ -1,13 +1,13 @@
-is_male = True
-is_tall = False
-
-if is_male and is_tall:
-    print("You are a male")
-elif is_male and not(is_tall):
-    print("Great")
-else:
-    print("Your are not male")
-
 # Comparisons
 def biggest_param(num1, num2, num3):
-    
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(biggest_param(3, 4, 5))
+
+
